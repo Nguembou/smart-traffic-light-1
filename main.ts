0fbebc1e0a2030aa6af3ac6ae8fbd2bf96fb5799
Nguembou <113910948+Nguembou@users.noreply.github.com>
2022-12-06 09:36:16 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -67,6 +67,7 @@ function red () {
     range_2 = strip.range(2, 1)
     range_2.showColor(neopixel.colors(NeoPixelColors.Black))
 }
+let distance = 0
 let range_2: neopixel.Strip = null
 let counter = 0
 let strip: neopixel.Strip = null
@@ -83,5 +84,15 @@ strip.setBrightness(70)
 counter = 9
 red()
 basic.forever(function () {
-	
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    control.waitMicros(2)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    control.waitMicros(10)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    distance += pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
+    basic.pause(500)
+    if (distance > 3.43) {
+        green()
+        basic.pause(5000)
+    }
 })
